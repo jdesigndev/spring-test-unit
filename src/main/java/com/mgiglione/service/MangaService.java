9fb81a9ec00556006f2a5f379f853dd3c0dd7dfc
Stephen Gorman <gormanst@leidos.com>
2020-11-09 18:09:57 -0500

    Removed a trailing slash from the search request url

--- src/main/java/com/mgiglione/service/MangaService.java
+++ src/main/java/com/mgiglione/service/MangaService.java
@@ -1,24 +1,15 @@
 package com.mgiglione.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import com.mgiglione.utils.RequestResponseLoggingInterceptor;
+import com.mgiglione.model.Manga;
+import com.mgiglione.model.MangaResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.mgiglione.model.Manga;
-import com.mgiglione.model.MangaResult;
+import java.util.List;
 
 @Service
 public class MangaService {
